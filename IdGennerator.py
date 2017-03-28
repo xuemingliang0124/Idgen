34a30a73de5b8866131e098d17140011d4ad011f
xuemingliang066@163.com <xuemingliang066@163.com>
2017-03-28 20:28:30 +0800

    IdGennerator

---
+++ IdGennerator.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('E:\\git\\IdGen')
+state=input('please input state:')
+city=input('please input city:')
+county=input('please input county:')
+year=input('please input year:')
+month=input('please input month:')
+day=input('please input day:')

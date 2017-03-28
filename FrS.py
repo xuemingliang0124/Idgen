bd74eb8f849ea9febbaa57b03f0e3df2d1bf05b7
xuemingliang0124 <xuemingliang066@163.com>
2017-03-29 00:57:46 +0800

    IDGen_v1

--- FrS.py
+++ FrS.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import os
-def GetStateNumeber(x,y,z):
+import random
+def GetNum(x,y,z):
     city1={}
     state={}
     county={}
@@ -15,3 +16,31 @@ def GetStateNumeber(x,y,z):
         if line[8:10]!='  ' :
             county[line[19:]]=line[8:10]
     return state[x]+city1[y]+county[z]
+def GetID(state,city,county,birthday,sex):
+    if sex=='男':
+        sex=random.randrange(1,9,2)
+    elif sex=='女':
+        sex=random.randrange(0,9,2)
+    else:
+        sex=input('input error,please input again:')
+    print(sex)
+    id1=str()
+    id1=GetNum(state,city,county)+birthday+str(random.randint(10,99))+str(sex)
+    
+    count = 0
+    weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2] #权重项
+    checkcode ={'0':'1','1':'0','2':'X','3':'9','4':'8','5':'7','6':'6','7':'5','8':'5','9':'3','10':'2'} #校验码映射
+    for i in range(0,len(id1)):
+        count = count +int(id1[i])*weight[i]
+    id1 = id1 + checkcode[str(count%11)] #算出校验码
+    return id1
+def PersonID():
+    state=input('please input state:')
+    city=input('please input city:')
+    county=input('please input county:')
+    birthday=input('please input birthday:')
+    sex=input('please input sex:')
+    ID=GetID(state,city,county,birthday,sex)
+    return ID
+Person=PersonID()
+print(Person)

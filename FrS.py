9770424780574802f79d2dada47dd3980687bbe5
xuemingliang066@163.com <xuemingliang066@163.com>
2017-03-29 13:16:38 +0800

    main

--- FrS.py
+++ FrS.py
@@ -1,31 +1,30 @@
 # coding=utf-8
 import os
 import random
-def GetNum(x,y,z):
-    city1={}
+def GetNum(x='北京市',y='市辖区',z='东城区'):
+    city={}
     state={}
     county={}
-    file=open('e:\\city_new.txt')
+    file=open('e:\\city2.txt')
     a=file.read()
     src1=a.split('\n')
     for line in src1:
         if line[:1]!=' ' :
             state[line[11:]]=line[0:2]
-        if line[:2]=='  ' and line[8:9]==' ':
-            city1[line[19:]]=line[4:6]
-        if line[8:10]!='  ' :
-            county[line[19:]]=line[8:10]
-    return state[x]+city1[y]+county[z]
-def GetID(state,city,county,birthday,sex):
+        if line[2:3]!=' ' and line[7:8]!=' ':
+            city[line[17:]]=line[4:6]
+        if line[8:9]!=' ' :
+            county[line[15:]]=line[8:10]
+    return state.get(x)+city.get(y)+county.get(z)
+def GetID(x,y,z,birthday,sex):
     if sex=='男':
         sex=random.randrange(1,9,2)
     elif sex=='女':
         sex=random.randrange(0,9,2)
     else:
         sex=input('input error,please input again:')
-    print(sex)
     id1=str()
-    id1=GetNum(state,city,county)+birthday+str(random.randint(10,99))+str(sex)
+    id1=GetNum(x,y,z)+birthday+str(random.randint(10,99))+str(sex)
     
     count = 0
     weight = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2] #权重项
@@ -35,12 +34,10 @@ def GetID(state,city,county,birthday,sex):
     id1 = id1 + checkcode[str(count%11)] #算出校验码
     return id1
 def PersonID():
-    state=input('please input state:')
-    city=input('please input city:')
-    county=input('please input county:')
+    x=input('please input state:')
+    y=input('please input city:')
+    z=input('please input county:')
     birthday=input('please input birthday:')
     sex=input('please input sex:')
-    ID=GetID(state,city,county,birthday,sex)
+    ID=GetID(x,y,z,birthday,sex)
     return ID
-Person=PersonID()
-print(Person)
